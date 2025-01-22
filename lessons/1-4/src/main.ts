e8ca17071a3e214b728c1df95779bbf9fe60dff4
yuichkun <yuichkun@gmail.com>
2025-01-23 06:20:31 +0900

    group objects

--- lessons/1-4/src/main.ts
+++ lessons/1-4/src/main.ts
@@ -2,11 +2,6 @@ import * as THREE from "three";
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: "red" });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-
 const sizes = {
   width: 800,
   height: 600,
@@ -20,5 +15,29 @@ scene.add(camera);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(sizes.width, sizes.height);
 document.body.appendChild(renderer.domElement);
+
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
+
+const group = new THREE.Group();
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+cube2.position.x = -2;
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube3.position.x = 2;
+group.add(cube1);
+group.add(cube2);
+group.add(cube3);
+group.position.y = 2;
+scene.add(group);
 
 renderer.render(scene, camera);
