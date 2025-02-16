b320aef03a128a9bf4c23f9bfe0b5d4425f28179
yuichkun <yuichkun@gmail.com>
2025-02-16 22:02:20 +0900

    use buffer geometry

--- lessons/1-8/src/main.ts
+++ lessons/1-8/src/main.ts
@@ -18,9 +18,38 @@ document.body.appendChild(renderer.domElement);
 
 scene.add(new THREE.AmbientLight(0xffffff, Math.PI / 2));
 
+const arr: number[] = [];
+
+const NUM_OF_TRIANGLES = 100;
+for (let i = 0; i < NUM_OF_TRIANGLES; i++) {
+  // origin
+  arr.push(0);
+  arr.push(0);
+  arr.push(0);
+
+  // other 2 points
+  for (let _ = 0; _ < 2; _++) {
+    const x = (Math.random() - 0.5) * 5;
+    const y = (Math.random() - 0.5) * 5;
+    const z = (Math.random() - 0.5) * 5;
+    arr.push(x);
+    arr.push(y);
+    arr.push(z);
+  }
+}
+console.log(arr);
+
+const positionsArray = new Float32Array(arr);
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
+const geometry = new THREE.BufferGeometry();
+geometry.setAttribute("position", positionsAttribute);
+
 const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshStandardMaterial({ color: "red" })
+  geometry,
+  new THREE.MeshStandardMaterial({
+    color: "#ff00ff",
+    wireframe: true,
+  })
 );
 scene.add(cube);
 
@@ -28,10 +57,17 @@ renderer.render(scene, camera);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
+let cameraAngle = 0;
+const initialCameraPos = camera.position.clone();
+const CAMERA_DISTANCE = initialCameraPos.length();
 function animate() {
   requestAnimationFrame(animate);
   controls.update();
   renderer.render(scene, camera);
+  const x = CAMERA_DISTANCE * Math.sin(cameraAngle);
+  const z = CAMERA_DISTANCE * Math.cos(cameraAngle);
+  camera.position.set(x, camera.position.y, z);
+  cameraAngle += 0.01;
 }
 
 animate();
